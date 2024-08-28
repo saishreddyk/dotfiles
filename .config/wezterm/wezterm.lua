4351ba414dee909dca72c5bddd13d250771a8a70
saishreddyk <saishreddy05@gmail.com>
2024-08-27 19:40:58 -0700

    modified wezterm, added blog, updated packages lock

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,4 +4,5 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 config.color_scheme = "Dracula (Official)"
 config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
+config.font_size = 13.0
 return config
